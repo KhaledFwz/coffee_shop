9ad5e980ee5ae06233b73c3e0c23404132d378e9
KhaledFwz <4everblackcat5000@gmail.com>
2025-04-19 18:44:53 +0200

    add static_root settings for colleccstatic code

--- Coffee_shop/settings.py
+++ Coffee_shop/settings.py
@@ -123,5 +123,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
